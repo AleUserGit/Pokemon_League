872ff8cbb5e03fc95a2fe2615de1a023860baac8
Ale <aleiannibelli@gmail.com>
2026-06-02 16:04:54 -0300

    Base proyecto

---
+++ funciones.py
@@ -0,0 +1,5 @@
+#Funcion 1 -- registrar
+#Funcion 2 -- eliminar
+#Funcion 3 -- modificar
+#Funcion 4 -- visualizar
+#Funcion 5 -- salir
---
+++ main.py
@@ -0,0 +1,2 @@
+# from funciones import <nombre funcion>
+# Llamen todas las funciones al main antes de arrancar
