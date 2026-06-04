d9e6293419c6e223b4b3e1ba4e7bd638a23f0238
Ale <aleiannibelli@gmail.com>
2026-06-04 15:04:09 -0300

    Estado general funciones

--- funciones.py
+++ funciones.py
@@ -1,22 +1,21 @@
-# from listas import lst_pokemones
 #Funcion 1 -- registrar pokemones
 
 def registrar_pokemon(lst_pokemones):
-    nombre = input("Ingrese nombre del Pokemón: ").strip()
+    nombre = input("Ingrese nombre del Pokemón: ").strip().capitalize()
     while nombre == "":
         print("Error: valor nulo")
-        nombre = input("Ingrese nombre del Pokemón").strip()
+        nombre = input("Ingrese nombre del Pokemón").strip().capitalize()
     
     tipos = ["Fuego", "Agua", "Planta", "Eléctrico", "Psíquico", "Lucha", "Roca", "Fantasma", "Dragón", "Normal"]
-    tipo = input("Ingrese el tipo del Pokemón: ").strip()
+    tipo = input("Ingrese el tipo del Pokemón: ").strip().capitalize()
     while tipo not in tipos:
         print("Error: tipo no válido")
-        tipo = input("Ingrese el tipo del Pokemón").strip()
+        tipo = input("Ingrese el tipo del Pokemón").strip().capitalize()
     
     nivel_valido = False
     while not nivel_valido:
         try:
-            nivel = int(input("Ingrese el nivel(1-100) del Pokemón: ")).strip()
+            nivel = int(input("Ingrese el nivel(1-100) del Pokemón: ").strip())
             if 1 <= nivel <= 100:
                 nivel_valido = True
             else:
@@ -27,7 +26,7 @@ def registrar_pokemon(lst_pokemones):
     poder_valido = False
     while not poder_valido:
         try:
-            poder = int(input("Ingrese el poder del Pokemón: ")).strip()
+            poder = int(input("Ingrese el poder del Pokemón: ").strip())
             if poder > 0:
                 poder_valido = True
             else:
@@ -45,7 +44,7 @@ def registrar_pokemon(lst_pokemones):
     victoria_valida = False
     while not victoria_valida:
         try:
-            victorias = int(input("Ingrese el número de victorias del Pokemón: ")).strip()
+            victorias = int(input("Ingrese el número de victorias del Pokemón: ").strip())
             if victorias >= 0:
                 victoria_valida = True
             else:
@@ -53,22 +52,31 @@ def registrar_pokemon(lst_pokemones):
         except ValueError:
             print("Error: no es un número entero")
 
-    estados_posibles = ["Disponible", "Entrenamiento", "Lesionado", "Liberado"]
-    estado = input("Ingrese el estado del Pokemón: ").strip()
-    while estado not in estado:
+    estado_valido = ["Disponible", "Entrenamiento", "Lesionado", "Liberado"]
+    estado = input("Ingrese el estado del Pokemón: ").strip().capitalize()
+    while estado not in estado_valido:
         print("Error: estado no válido")
-        estado = input("Ingrese el estado del Pokemón: ").strip()
+        estado = input("Ingrese el estado del Pokemón: ").strip().capitalize()
 
 
 
-
-
-    lst_pokemones.append(nombre, tipo, nivel, poder, entrenador, victorias, estado)
+    lst_pokemones.append([nombre, tipo, nivel, poder, entrenador, victorias, estado])
 
 
 
 #Funcion 2 -- eliminar
-
+def eliminar_pokemon(lst_pokemones):
+    nombre = input("Ingrese el nombre del Pokemon a eliminar: ").strip()
+    '''
+    Buscar en la lista
+    Si existe y es "liberado" pedir confirmación
+        eliminar
+    Si no existe
+        avisar
+    Si existe pero no está liberado
+        avisar 
+    '''
+    
 
 #Funcion 3 -- modificar
 #Funcion 4 -- visualizar
