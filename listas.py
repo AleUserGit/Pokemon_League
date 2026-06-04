432c233c0748b3c72eae07e2931cb4813345b571
Ale <aleiannibelli@gmail.com>
2026-06-04 16:58:46 -0300

    Poder Listas

--- listas.py
+++ listas.py
@@ -6,47 +6,47 @@
 # lst_pokemones([nombre, tipo, nivel, poder, entrenador, victorias, estado])
 
 lst_pokemones = [
-    ["Bulbasaur", "Planta", 45, 49, 49, 45],
-    ["Ivysaur", "Planta", 60, 62, 63, 60],
-    ["Venusaur", "Planta", 80, 82, 83, 80],
+    ["Bulbasaur", "Planta", 45, 1880, 49, 45],
+    ["Ivysaur", "Planta", 60, 2450, 63, 60],
+    ["Venusaur", "Planta", 80, 3250, 83, 80],
 
-    ["Charmander", "Fuego", 39, 52, 43, 65],
-    ["Charmeleon", "Fuego", 58, 64, 58, 80],
-    ["Charizard", "Fuego", 78, 84, 78, 100],
+    ["Charmander", "Fuego", 39, 1990, 43, 65],
+    ["Charmeleon", "Fuego", 58, 2600, 58, 80],
+    ["Charizard", "Fuego", 78, 3400, 78, 100],
 
-    ["Squirtle", "Agua", 44, 48, 65, 43],
-    ["Wartortle", "Agua", 59, 63, 80, 58],
-    ["Blastoise", "Agua", 79, 83, 100, 78],
+    ["Squirtle", "Agua", 44, 2000, 65, 43],
+    ["Wartortle", "Agua", 59, 2600, 80, 58],
+    ["Blastoise", "Agua", 79, 3400, 100, 78],
 
-    ["Pikachu", "Eléctrico", 35, 55, 40, 90],
-    ["Raichu", "Eléctrico", 60, 90, 55, 110],
+    ["Pikachu", "Eléctrico", 35, 2200, 40, 90],
+    ["Raichu", "Eléctrico", 60, 3150, 55, 110],
 
-    ["Jigglypuff", "Normal", 90, 45, 20, 20],
-    ["Meowth", "Normal", 40, 45, 35, 90],
+    ["Jigglypuff", "Normal", 90, 1750, 20, 20],
+    ["Meowth", "Normal", 40, 2100, 35, 90],
 
-    ["Psyduck", "Agua", 50, 52, 48, 55],
-    ["Machop", "Lucha", 70, 80, 50, 35],
+    ["Psyduck", "Agua", 50, 2050, 48, 55],
+    ["Machop", "Lucha", 70, 2350, 50, 35],
 
-    ["Geodude", "Roca", 40, 80, 100, 20],
-    ["Gastly", "Fantasma", 20, 35, 30, 80],
+    ["Geodude", "Roca", 40, 2400, 100, 20],
+    ["Gastly", "Fantasma", 20, 1750, 30, 80],
 
-    ["Onix", "Roca", 25, 45, 160, 70],
+    ["Onix", "Roca", 25, 3100, 160, 70],
 
-    ["Cubone", "Roca", 40, 50, 95, 35],
+    ["Cubone", "Roca", 40, 2300, 95, 35],
 
-    ["Magikarp", "Agua", 10, 10, 55, 80],
-    ["Gyarados", "Agua", 85, 125, 79, 81],
+    ["Magikarp", "Agua", 10, 1650, 55, 80],
+    ["Gyarados", "Agua", 85, 3800, 79, 81],
 
-    ["Snorlax", "Normal", 90, 110, 65, 30],
+    ["Snorlax", "Normal", 90, 3650, 65, 30],
 
-    ["Articuno", "Agua", 80, 85, 100, 85],
-    ["Zapdos", "Eléctrico", 80, 90, 85, 100],
-    ["Moltres", "Fuego", 80, 100, 90, 90],
+    ["Articuno", "Agua", 80, 3600, 100, 85],
+    ["Zapdos", "Eléctrico", 80, 3650, 85, 100],
+    ["Moltres", "Fuego", 80, 3700, 90, 90],
 
-    ["Dratini", "Dragon", 31, 64, 45, 50],
-    ["Dragonair", "Dragon", 51, 84, 65, 70],
-    ["Dragonite", "Dragon", 81, 134, 95, 80],
+    ["Dratini", "Dragon", 31, 2000, 45, 50],
+    ["Dragonair", "Dragon", 51, 2800, 65, 70],
+    ["Dragonite", "Dragon", 81, 4000, 95, 80],
 
-    ["Mewtwo", "Psíquico", 66, 110, 90, 130],
-    ["Mew", "Psíquico", 67, 100, 100, 100]
+    ["Mewtwo", "Psíquico", 66, 4360, 90, 130],
+    ["Mew", "Psíquico", 67, 4000, 100, 100]
 ]
